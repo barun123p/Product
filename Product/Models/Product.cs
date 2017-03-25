dfa064ffc101fa571ecd3b8d134ec75625ce287e
barunp@gmail.com <Barun@LAPTOP-K0DF4E82>
2017-03-25 19:39:42 -0400

    Add project files.

---
+++ Product/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Product.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+  
+        public string Name { get; set; }
+
+        public string Category { get; set; }
+
+        public int Price { get; set; }
+
+       
+    }
+}
